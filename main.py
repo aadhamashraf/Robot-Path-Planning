25961273988890fa8043e3cfc1cd1e1be1167d0f
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-11-13 10:05:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import pygame
 import sys
-# import pandas as pd
 import os 
 import os.path
 import time 
 import random 
-
-from environment import Cell, generate_maze
+import matplotlib.pyplot as plt
+from environment import Cell, generate_maze , export_frontier , draw_button , draw_path , showDifferences_ExcutionTime
 from Searching_Algorithms import Uninformed_Search, Local_Search, Heuristic_Search
 from collections import deque
+plt.style.use('dark_background')
 
 compareAlgos = {
-    "BFS" : [0], 
-    "DFS" : [0], 
-    "UCS" : [0],
-    "IDS" : [0],
-    "Greedy BFS" :[0] ,
-    "A*" :[0],
-    "Hill Climbing Stairs":[0],
-    "Simulated Annealing":[0],
-    "Genetic Algos":[0]
+    # [Time]
+    "BFS" : [], 
+    "DFS" : [], 
+    "UCS" : [],
+    "IDS" : [],
+    "Greedy BFS" :[] ,
+    "A*" :[],
+    "Hill Climbing Stairs":[],
+    "Simulated Annealing":[],
+    "Genetic Algos":[]
 }
 
 DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-WIDTH, HEIGHT = 1300, 640
+WIDTH, HEIGHT = 1300, 650
 CELL_SIZE = 20
 MAZE_WIDTH = WIDTH // CELL_SIZE - 10
 MAZE_HEIGHT = HEIGHT // CELL_SIZE
@@ -36,35 +37,6 @@ BUTTON_BORDER_COLOR = (0, 0, 0)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def export_frontier(frontier , algorithm):
-    dir = rf"{os.getcwd()}\Forntier Results" 
-
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-    
-    with open(os.path.join(dir, f"frontier_{algorithm}.txt"), "w") as f:
-        for node in frontier:
-            f.write(f"{node}\n")
-
-def draw_button(screen, text, x, y, width, height):
-    font = pygame.font.Font(None, 20)
-    pygame.draw.rect(screen, BUTTON_COLOR, (x, y, width, height))
-    pygame.draw.rect(screen, BUTTON_BORDER_COLOR, (x, y, width, height), 2)
-    
-    if ' ' in text: 
-        lines = text.split(' ', 1)
-        line1 = font.render(lines[0], True, BLACK)
-        line2 = font.render(lines[1], True, BLACK)
-        screen.blit(line1, (x + (width - line1.get_width()) // 2, y + 5))
-        screen.blit(line2, (x + (width - line2.get_width()) // 2, y + height // 2 + 5))
-    else:
-        text_surface = font.render(text, True, BLACK)
-        text_rect = text_surface.get_rect(center=(x + width // 2, y + height // 2))
-        screen.blit(text_surface, text_rect)
-
-def draw_path(path):
-    for node in path:
-        pygame.draw.rect(screen, GREEN, (node[0] * CELL_SIZE, node[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
 def main():
     grid = [[Cell(x, y) for x in range(MAZE_WIDTH)] for y in range(MAZE_HEIGHT)]
@@ -104,6 +76,7 @@ def main():
         draw_button(screen, 'Hill Climbing', button_x, button_y + 6 * button_gap, button_width, button_height)
         draw_button(screen, 'Simulated Annealing', button_x, button_y + 7 * button_gap, button_width, button_height)
         draw_button(screen, 'Genetic Algos', button_x, button_y + 8 * button_gap, button_width, button_height)
+        draw_button(screen, 'Compare Excution Time', button_x, button_y + 9 * button_gap, button_width, button_height)
 
         # Event handling
         for event in pygame.event.get():
@@ -129,58 +102,73 @@ def main():
                     selected_algorithm = 'Simulated Annealing'
                 elif button_x <= x <= button_x + button_width and button_y + 8 * button_gap <= y <= button_y + 8 * button_gap + button_height:
                     selected_algorithm = 'Genetic Algos'
-            
+                elif button_x <= x <= button_x + button_width and button_y + 9 * button_gap <= y <= button_y + 9 * button_gap + button_height:
+                    showDifferences_ExcutionTime(compareAlgos)
+
         if selected_algorithm == 'BFS':
             path, frontier, step_count , timeTaken = Uninformed_Search.bfs(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'BFS')
-
+            compareAlgos['BFS'].append(timeTaken)
+           
         elif selected_algorithm == 'DFS':
             path, frontier, step_count, timeTaken = Uninformed_Search.dfs(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'DFS')
-        
+            compareAlgos['DFS'].append(timeTaken)
+
         elif selected_algorithm == 'A*':
             path, frontier, step_count , timeTaken= Heuristic_Search.a_star(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'A*')
-        
+            compareAlgos['A*'].append(timeTaken)
+
         elif selected_algorithm == 'UCS':
             path, frontier, step_count, timeTaken = Uninformed_Search.ucs(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'UCS')
+            compareAlgos['UCS'].append(timeTaken)
+
         
         elif selected_algorithm == 'IDS':
             path, frontier, step_count, timeTaken = Uninformed_Search.ids(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'IDS')
+            compareAlgos['IDS'].append(timeTaken)
+
         
         elif selected_algorithm == 'Greedy BFS':
             path, frontier, step_count, timeTaken = Heuristic_Search.greedy_bfs(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'Greedy BFS')
+            compareAlgos['Greedy BFS'].append(timeTaken)
+
         
         elif selected_algorithm == 'Hill Climbing':
             path, frontier, step_count, timeTaken = Local_Search.hill_climbing(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'Hill Climbing')
+            compareAlgos['Hill Climbing'].append(timeTaken)
+
         
         elif selected_algorithm == 'Simulated Annealing':
             path, frontier, step_count, timeTaken = Local_Search.simulated_annealing(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'Simulated Annealing')
+            compareAlgos['Simulated Annealing'].append(timeTaken)
+
         
         elif selected_algorithm == 'Genetic Algos':
             path, frontier, step_count, timeTaken = Local_Search.genetic_algorithm(start, goal, grid)
             draw_path(path)
             export_frontier(frontier , 'Genetic Algos')
+            compareAlgos['Genetic Algos'].append(timeTaken)
+
 
         font = pygame.font.Font(None, 25)
         text = font.render(f"Steps: {step_count}", True, BLACK)
-        time_taken = font.render(f"Time Taken : {timeTaken* 1e-2}",True , BLACK)
-
-        screen.blit(text, (button_x, button_y + 9 * button_gap))
-        screen.blit(time_taken, (button_x, button_y + 9.5 * button_gap))
+        
+        screen.blit(text, (button_x, button_y + 10 * button_gap))
 
         pygame.display.flip()
 
