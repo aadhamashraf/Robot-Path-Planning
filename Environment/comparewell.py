0be10769945006883bbd20650a831251cc23a208
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2024-11-16 07:13:11 +0200

    Update comparewell.py

--- Environment/comparewell.py
+++ Environment/comparewell.py
@@ -1,1 +1,23 @@
+from Basic_Attributes import * 
 
+def export_frontier(frontier , algorithm):
+    dir = rf"{os.getcwd()}\Forntier Results" 
+
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    
+    with open(os.path.join(dir, f"frontier_{algorithm}.txt"), "w") as f:
+        for node in frontier:
+            f.write(f"{node}\n")
+            
+def showDifferences_ExecutionTime(compareAlgos):
+    sorted_compareAlgos = dict(sorted(compareAlgos.items(), key=lambda item: item[1]))
+    
+    plt.figure(figsize=(10, 10))
+    plt.barh(list(sorted_compareAlgos.keys()), list(sorted_compareAlgos.values()))
+    plt.title("Searching Algorithms Execution Time")
+    plt.xlabel("Execution Time")
+    plt.ylabel("Searching Algorithm")
+    plt.grid(True)
+    plt.show()
+    
